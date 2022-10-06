47523689e13c8e2e852f94fd4899d4ecb57c73c6
Jayshree Upadhyay <51030227+Jaaishri@users.noreply.github.com>
2022-10-06 12:42:11 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -100,7 +100,7 @@ let resumeButton = document.getElementById('resume-button')
 // Html2pdf options
 let opt = {
 	margin:       0,
-	filename:     'JayshreeCV-Teacher.pdf',
+	filename:     'JayshreeCV.pdf',
 	image:        { type: 'jpeg', quality: 0.98 },
 	html2canvas:  { scale: 2 },
 	jsPDF:        { format: 'a4', orientation: 'portrait' }
